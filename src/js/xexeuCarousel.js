1cfe7de8464687c6879c6f1d9d7b6ceaacdaef07
Jhonny Michel <jhonnymichel@outlook.com>
2017-10-15 21:41:33 -0300

    basic carousel logic

--- src/js/xexeuCarousel.js
+++ src/js/xexeuCarousel.js
@@ -1,16 +1,62 @@
 'use strict';
+import XexeuCarouselItem from './xexeuCarouselItem.js';
 
 class XexeuCarousel extends HTMLElement {
   static get ELEMENT_NAME() {
     return 'xexeu-carousel';
   }
 
-  connectedCallback() {
-    console.log('element is in the DOM');
+  constructor() {
+    super();
+    this.style.cssText = `
+      display: block;
+      overflow: hidden;
+      position: relative;
+      width: 100%;
+      height: 100%;
+    `;
   }
 
-  attributeChangedCallback(attributeName, oldValue, newValue, namespace) {
+  connectedCallback() {
+    this.startupCarousel();
+    setInterval(() => {
+      this.nextCarouselItem();
+    }, 3000)
+  }
 
+  startupCarousel() {
+    const children = Array.from(this.children);
+    const validChildren = children.filter(this.isValidChild);
+    this.carouselItems = validChildren;
+    this.currentDisplayingItem = validChildren[0];
+    const invalidChildren = children.filter((child) => !this.isValidChild(child));
+    invalidChildren.forEach((child) => {
+      console.warn('XexeuCarousel: a invalid child is found and will be removed. Only xexeu-carousel-item is allowed as a top-level child.')
+      return this.removeChild(child);
+    });
+  }
+
+  nextCarouselItem() {
+    const nextItem = this.currentDisplayingItem.nextElementSibling || this.carouselItems[0];
+    this.currentDisplayingItem = nextItem;
+  }
+
+  previousCarouselItem() {
+    const previousItem = this.currentDisplayingItem.previousElementSibling || this.carouselItems[this.carouselItems.length-1];
+    this.currentDisplayingItem = previousItem;
+  }
+
+  isValidChild(child) {
+    return child.tagName.toLowerCase() === XexeuCarouselItem.ELEMENT_NAME;
+  }
+
+  set currentDisplayingItem(item) {
+    this.carouselItems.forEach(item => item.removeAttribute('displaying'));
+    item.setAttribute('displaying', '');
+  }
+
+  get currentDisplayingItem() {
+    return this.carouselItems.find(item => item.hasAttribute('displaying'));
   }
 }
 
--- src/js/xexeuCarouselItem.js
+++ src/js/xexeuCarouselItem.js
@@ -6,12 +6,63 @@ class XexeuCarouselItem extends HTMLElement {
     return 'xexeu-carousel-item';
   }
 
+  static get observedAttributes() {
+    return ['displaying'];
+  }
+
+  constructor() {
+    super();
+  }
+
   connectedCallback() {
-    console.log('element is in the DOM');
+    this.style.cssText = `
+      display: none;
+      position: relative;
+      width: 100%;
+      height: 100%;
+      align-items: center;
+      justify-content: center;
+      transition: transform ${this.transitionTime}s ${this.transitionType};
+    `;
+
+    if (this.hasAttribute('displaying')) {
+      this.showObject();
+    }
+  }
+
+  showObject() {
+    this.style.display = 'flex';
+  }
+
+  hideObject() {
+    this.style.display = 'none';
   }
 
   attributeChangedCallback(attributeName, oldValue, newValue, namespace) {
+    switch (attributeName) {
+      case 'displaying':
+        if (newValue === "") {
+          this.showObject();
+        } else {
+          this.hideObject();
+        }
+    }
+  }
 
+  get transitionTime() {
+    return (
+      this.getAttribute('transition-time') ||
+      this.parentElement.getAttribute('transition-time') ||
+      .3
+    );
+  }
+
+  get transitionType() {
+    return (
+      this.getAttribute('transition-type') ||
+      this.parentElement.getAttribute('transition-type') ||
+      'ease-in-out'
+    );
   }
 }
 
